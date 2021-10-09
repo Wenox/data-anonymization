fec75312e0034e8cae7c254c7f8301825a4666ea
Wenox <wenoxar@gmail.com>
2021-10-09 17:55:31 +0200

    feat: maven as a build tool

---
+++ backend/src/test/java/com/wenox/anonymization/AnonymizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenox.anonymization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
