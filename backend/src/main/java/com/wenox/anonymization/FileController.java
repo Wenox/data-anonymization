991030f509f7cff4e24a02146c369e98d479ec8b
Wenox <wenoxar@gmail.com>
2021-11-06 12:48:16 +0100

    feat: restoring database

--- backend/src/main/java/com/wenox/anonymization/FileController.java
+++ backend/src/main/java/com/wenox/anonymization/FileController.java
@@ -1,6 +1,8 @@
 package com.wenox.anonymization;
 
 import java.io.IOException;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,29 +12,45 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileController {
 
   private final FileUploader fileUploader;
+  private final ApplicationEventPublisher publisher;
 
-  public FileController(final FileUploader fileUploader) {
+  public FileController(final FileUploader fileUploader, final ApplicationEventPublisher publisher) {
     this.fileUploader = fileUploader;
+    this.publisher = publisher;
   }
 
   @PostMapping("/api/v1/files")
   public void upload(@RequestParam("file") MultipartFile file, @RequestParam("type") FileType type) {
-    System.out.println("Inside /api/v1/files");
-    System.out.println("Type: " + type);
-
-    System.out.println(file);
-
-    System.out.println("file name: " + file.getName());
-    System.out.println("file original name: " + file.getOriginalFilename());
-    System.out.println("file content type: " + file.getContentType());
-    System.out.println("file size: " + file.getSize());
-
     try {
-      fileUploader.upload(file, type);
+      final var savedFile = fileUploader.upload(file, type);
+      publisher.publishEvent(new FileUploadedSuccessEvent(this, savedFile));
     } catch (final IOException ex) {
       ex.printStackTrace();
+      publisher.publishEvent(new FileUploadedFailureEvent(this));
     }
 
     System.out.println("All successful");
+
+  }
+
+  static class FileUploadedSuccessEvent extends ApplicationEvent {
+
+    private FileEntity file;
+
+    public FileUploadedSuccessEvent(Object source, FileEntity file) {
+      super(source);
+      this.file = file;
+    }
+
+    public FileEntity getFile() {
+      return file;
+    }
+  }
+
+  static class FileUploadedFailureEvent extends ApplicationEvent {
+
+    public FileUploadedFailureEvent(Object source) {
+      super(source);
+    }
   }
 }
