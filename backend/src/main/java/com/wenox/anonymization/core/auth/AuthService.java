9966b4cfbeb48502f086c034c5528d6d2fb3fbd0
Wenox <wenoxar@gmail.com>
2021-11-20 17:49:45 +0100

    feat: bcrypt password encoder

--- backend/src/main/java/com/wenox/anonymization/core/auth/AuthService.java
+++ backend/src/main/java/com/wenox/anonymization/core/auth/AuthService.java
@@ -2,21 +2,24 @@ package com.wenox.anonymization.core.auth;
 
 import com.wenox.anonymization.core.ApiResponse;
 import com.wenox.anonymization.core.domain.Role;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
   private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
-  public AuthService(UserRepository userRepository) {
+  public AuthService(UserRepository userRepository,PasswordEncoder passwordEncoder) {
     this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
   }
 
   public ApiResponse register(RegisterRequest dto) {
     User user = new User();
     user.setEmail(dto.getEmail());
-    user.setPassword(dto.getPassword());
+    user.setPassword(passwordEncoder.encode(dto.getPassword()));
     user.setRole(Role.USER);
     user.setBlocked(false);
 
