74996210fbb104ef87157b5fe4fe655c26fb0691
Wenox <wenoxar@gmail.com>
2021-12-12 18:01:00 +0100

    feat: refresh tokens service

--- backend/src/main/java/com/wenox/anonymization/core/controller/AuthController.java
+++ backend/src/main/java/com/wenox/anonymization/core/controller/AuthController.java
@@ -1,9 +1,15 @@
 package com.wenox.anonymization.core.controller;
 
+
 import com.wenox.anonymization.core.dto.ApiResponse;
+import com.wenox.anonymization.core.security.service.RefreshTokenService;
 import com.wenox.anonymization.core.service.AuthService;
 import com.wenox.anonymization.core.dto.UserRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,14 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
+  private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
   private final AuthService authService;
 
-  public AuthController(AuthService authService) {
+  private final RefreshTokenService refreshTokenService;
+
+  public AuthController(AuthService authService, RefreshTokenService refreshTokenService) {
     this.authService = authService;
+    this.refreshTokenService = refreshTokenService;
   }
 
   @PostMapping("/register")
   public ResponseEntity<ApiResponse> register(@Valid @RequestBody UserRequest dto) {
     return ResponseEntity.ok(authService.register(dto));
+  }
+
+  @PostMapping("/refresh-token")
+  public void refreshToken(HttpServletRequest request, HttpServletResponse response) {
+    refreshTokenService.generateTokens(request, response);
   }
 }
