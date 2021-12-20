af49223da76036f917d942b9412d49d13506b679
Wenox <wenoxar@gmail.com>
2021-12-20 02:15:49 +0100

    feat: wip: users table

--- frontend/src/pages/users.tsx
+++ frontend/src/pages/users.tsx
@@ -2,25 +2,44 @@ import {useQuery} from "react-query";
 import {getUsers, User} from "../api/users";
 import {DataGrid, GridColDef} from "@mui/x-data-grid";
 import '../styles/users.scss';
+import {IconButton} from "@mui/material";
+import {Block, Edit, HowToReg} from "@mui/icons-material";
 
 const Users = () => {
 
-  const { data } = useQuery('users', getUsers);
+  const { data, isLoading } = useQuery('users', getUsers);
   const users: User[] = data?.data || [];
 
   const columns: GridColDef[] = [
-    { field: 'email', headerName: 'E-mail address' },
-    { field: 'blocked', headerName: 'Is blocked?' },
-    { field: 'role', headerName: 'Role' },
+    { field: 'email', headerName: 'E-mail address', flex: 1},
+    { field: 'blocked', headerName: 'Is blocked?', flex: 1,
+      renderCell: params => <span>{params.row.blocked ? 'Blocked' : 'Active'}</span>
+    },
+    { field: 'role', headerName: 'Role', flex: 1 },
+    {field: 'actions', headerName: 'Actions', width: 125,
+      sortable: false,
+      filterable: false,
+      renderCell: params => {
+        return (
+          <div>
+            <IconButton onClick={() => {}}>
+              <Edit/>
+            </IconButton>
+            <IconButton onClick={() => {}}>
+              {params.row.blocked ? <HowToReg color='primary' /> : <Block color='secondary' />}
+            </IconButton>
+          </div>
+        );
+      }
+    }
   ];
 
   return (
     <>
-      <div id='user-registration' style={{ height: 500 }}>
+      <div id='user-registration'>
         <h1>Users</h1>
-        <DataGrid columns={columns} rows={users} />
+        <DataGrid autoHeight columns={columns} rows={users} loading={isLoading} />
       </div>
-
     </>
   )
 }
