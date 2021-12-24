587c9c83f1042fb3ae36b87936b78caf7513aa2d
Wenox <wenoxar@gmail.com>
2021-12-24 03:42:51 +0100

    refactor: adjust backend to new roles

--- backend/src/main/java/com/wenox/anonymization/core/domain/FileType.java
+++ backend/src/main/java/com/wenox/anonymization/core/domain/FileType.java
@@ -1,8 +1,6 @@
 package com.wenox.anonymization.core.domain;
 
 public enum FileType {
-  JSON,
-  CSV,
   PSQL,
   MYSQL
 }
--- backend/src/main/java/com/wenox/anonymization/uploader/core/FileRepository.java
+++ backend/src/main/java/com/wenox/anonymization/uploader/core/FileRepository.java
@@ -1,6 +1,5 @@
 package com.wenox.anonymization.uploader.core;
 
-import com.wenox.anonymization.uploader.core.FileEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
