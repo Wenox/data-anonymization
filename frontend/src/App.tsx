144216374f4bfa08fab8f8a0d4feba0c7e133700
Wenox <wenoxar@gmail.com>
2021-12-25 05:52:18 +0100

    feat: configurable structure for routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,15 @@
 import React, { FC, useEffect, useState } from 'react';
-import Main from './pages/main';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import About from './pages/about';
-import Login from './pages/login';
-import Register from './pages/register';
 import { Container, ThemeProvider } from '@mui/material';
 import PrivateRoute from './components/private-route';
 import { theme } from './styles/theme';
-import ResetPassword from './pages/reset-password/reset-password';
-import ChangePasswordLoading from './pages/reset-password/change-password-loading';
-import ChangePasswordForm from './pages/reset-password/change-password-form';
-import InvalidPasswordToken from './pages/reset-password/tokens/invalid-password-token';
-import ExpiredPasswordToken from './pages/reset-password/tokens/expired-password-token';
-import ConsumedPasswordToken from './pages/reset-password/tokens/consumed-password-token';
-import Users from './pages/users';
 import AuthContext from './context/auth-context';
 import CssBaseline from '@mui/material/CssBaseline';
 import { MeResponse } from './api/requests/me/me.types';
 import { getMe } from './api/requests/me/me.requests';
-import VerifyMailLoading from './pages/verify-mail/verify-mail-loading';
-import ExpiredVerifyToken from './pages/verify-mail/tokens/expired-verify-token';
-import InvalidVerifyToken from './pages/verify-mail/tokens/invalid-verify-token';
-import UserAlreadyVerified from './pages/verify-mail/tokens/user-already-verified';
-import UserVerifySuccess from './pages/verify-mail/tokens/user-verify-success';
-import VerifyMailPrompt from './pages/verify-mail/verify-mail-prompt';
-import ResentVerifyToken from './pages/verify-mail/tokens/resent-verify-token';
-import MiniDrawer from './components/mini-drawer';
+import Navigation from './components/navigation/navigation';
+import { MAIN_ROUTES } from './constants/routes';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -58,38 +41,48 @@ const App: FC = () => {
               {!isLoading && (
                 <>
                   <CssBaseline />
+
                   <Routes>
-                    <Route path="/" element={<PrivateRoute />}>
-                      <Route path="/" element={<Main />} />
-                    </Route>
-                    <Route path="/about" element={<PrivateRoute />}>
-                      <Route path="/about" element={<About />} />
-                    </Route>
-                    <Route path="/users" element={<PrivateRoute />}>
-                      <Route
-                        path="/users"
-                        element={
-                          <MiniDrawer>
-                            <Users />
-                          </MiniDrawer>
-                        }
-                      />
-                    </Route>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/reset-password" element={<ResetPassword />} />
-                    <Route path="/change-password" element={<ChangePasswordLoading />} />
-                    <Route path="/change-password/form" element={<ChangePasswordForm />} />
-                    <Route path="/change-password/invalid-token" element={<InvalidPasswordToken />} />
-                    <Route path="/change-password/expired-token" element={<ExpiredPasswordToken />} />
-                    <Route path="/change-password/consumed-token" element={<ConsumedPasswordToken />} />
-                    <Route path="/verify-mail" element={<VerifyMailLoading />} />
-                    <Route path="/verify-mail-prompt" element={<VerifyMailPrompt />} />
-                    <Route path="/verify-mail/success" element={<UserVerifySuccess />} />
-                    <Route path="/verify-mail/invalid-token" element={<InvalidVerifyToken />} />
-                    <Route path="/verify-mail/expired-token" element={<ExpiredVerifyToken />} />
-                    <Route path="/verify-mail/already-verified" element={<UserAlreadyVerified />} />
-                    <Route path="/verify-mail/token-sent-again" element={<ResentVerifyToken />} />
+                    {MAIN_ROUTES.map((route) => {
+                      const Element = route.element;
+
+                      if (route.authenticated) {
+                        return (
+                          <Route key={route.path} path={route.path} element={<PrivateRoute />}>
+                            <Route
+                              key={route.path}
+                              path={route.path}
+                              element={
+                                route.menu ? (
+                                  <Navigation>
+                                    <Element />
+                                  </Navigation>
+                                ) : (
+                                  <Element />
+                                )
+                              }
+                            />
+                          </Route>
+                        );
+                      } else {
+                        return (
+                          <Route
+                            key={route.path}
+                            path={route.path}
+                            element={
+                              route.menu ? (
+                                <Navigation>
+                                  <Element />
+                                </Navigation>
+                              ) : (
+                                <Element />
+                              )
+                            }
+                          />
+                        );
+                      }
+                    })}
+
                     <Route path="*" element={<Navigate to="/login" />} />
                   </Routes>
                 </>
---
+++ frontend/src/constants/routes.ts
@@ -0,0 +1,159 @@
+import Login from '../pages/login';
+import Register from '../pages/register';
+import About from '../pages/about';
+import Users from '../pages/users';
+import Main from '../pages/main';
+import ResetPassword from '../pages/reset-password/reset-password';
+import ChangePasswordLoading from '../pages/reset-password/change-password-loading';
+import ChangePasswordForm from '../pages/reset-password/change-password-form';
+import InvalidPasswordToken from '../pages/reset-password/tokens/invalid-password-token';
+import ExpiredPasswordToken from '../pages/reset-password/tokens/expired-password-token';
+import ConsumedPasswordToken from '../pages/reset-password/tokens/consumed-password-token';
+import VerifyMailLoading from '../pages/verify-mail/verify-mail-loading';
+import VerifyMailPrompt from '../pages/verify-mail/verify-mail-prompt';
+import UserVerifySuccess from '../pages/verify-mail/tokens/user-verify-success';
+import InvalidVerifyToken from '../pages/verify-mail/tokens/invalid-verify-token';
+import ExpiredVerifyToken from '../pages/verify-mail/tokens/expired-verify-token';
+import UserAlreadyVerified from '../pages/verify-mail/tokens/user-already-verified';
+import ResentVerifyToken from '../pages/verify-mail/tokens/resent-verify-token';
+import React from 'react';
+
+export const ROUTES = {
+  LOGIN: '/login',
+  REGISTER: '/register',
+  USERS: '/users',
+  MAIN: '/',
+  ABOUT: '/about',
+  RESET_PASSWORD: '/reset-password',
+  CHANGE_PASSWORD: '/change-password',
+  CHANGE_PASSWORD_FORM: '/change-password/form',
+  CHANGE_PASSWORD_INVALID_TOKEN: '/change-password/invalid-token',
+  CHANGE_PASSWORD_EXPIRED_TOKEN: '/change-password/expired-token',
+  CHANGE_PASSWORD_CONSUMED_TOKEN: '/change-password/consumed-token',
+  CHANGE_PASSWORD_INVALID: '/change-password/invalid-token',
+  VERIFY_MAIL: '/verify-mail',
+  VERIFY_MAIL_PROMPT: '/verify-mail-prompt',
+  VERIFY_MAIL_SUCCESS: '/verify-mail/success',
+  VERIFY_MAIL_INVALID_TOKEN: '/verify-mail/invalid-token',
+  VERIFY_MAIL_EXPIRED_TOKEN: '/verify-mail/expired-token',
+  VERIFY_MAIL_ALREADY_VERIFIED: '/verify-mail/already-verified',
+  VERIFY_MAIL_TOKEN_SENT_AGAIN: '/verify-mail/token-sent-again',
+};
+
+export interface RouteDescription {
+  path: string;
+  element: React.FunctionComponent;
+  authenticated: boolean;
+  menu: boolean;
+}
+
+export const MAIN_ROUTES: RouteDescription[] = [
+  {
+    path: ROUTES.LOGIN,
+    element: Login,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.REGISTER,
+    element: Register,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.ABOUT,
+    element: About,
+    authenticated: false,
+    menu: true,
+  },
+  {
+    path: ROUTES.USERS,
+    element: Users,
+    authenticated: true,
+    menu: true,
+  },
+  {
+    path: ROUTES.MAIN,
+    element: Main,
+    authenticated: true,
+    menu: false,
+  },
+  {
+    path: ROUTES.RESET_PASSWORD,
+    element: ResetPassword,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.CHANGE_PASSWORD,
+    element: ChangePasswordLoading,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.CHANGE_PASSWORD_FORM,
+    element: ChangePasswordForm,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.CHANGE_PASSWORD_INVALID_TOKEN,
+    element: InvalidPasswordToken,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.CHANGE_PASSWORD_EXPIRED_TOKEN,
+    element: ExpiredPasswordToken,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.CHANGE_PASSWORD_CONSUMED_TOKEN,
+    element: ConsumedPasswordToken,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.VERIFY_MAIL,
+    element: VerifyMailLoading,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.VERIFY_MAIL_PROMPT,
+    element: VerifyMailPrompt,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.VERIFY_MAIL_SUCCESS,
+    element: UserVerifySuccess,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.VERIFY_MAIL_INVALID_TOKEN,
+    element: InvalidVerifyToken,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.VERIFY_MAIL_EXPIRED_TOKEN,
+    element: ExpiredVerifyToken,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.VERIFY_MAIL_ALREADY_VERIFIED,
+    element: UserAlreadyVerified,
+    authenticated: false,
+    menu: false,
+  },
+  {
+    path: ROUTES.VERIFY_MAIL_TOKEN_SENT_AGAIN,
+    element: ResentVerifyToken,
+    authenticated: false,
+    menu: false,
+  },
+];
