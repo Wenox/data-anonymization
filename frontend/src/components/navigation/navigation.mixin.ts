f874121bb9f59aef2756ac8aa25986420477dc70
Wenox <wenoxar@gmail.com>
2021-12-25 05:45:07 +0100

    feat: navigation

---
+++ frontend/src/components/navigation/navigation.mixin.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+import { CSSObject, Theme } from '@mui/material/styles';
+
+export const navigationWidth = 240;
+
+export const navigationOpenedMixin = (theme: Theme): CSSObject => ({
+  width: navigationWidth,
+  overflowX: 'hidden',
+});
+
+export const navigationClosedMixin = (theme: Theme): CSSObject => ({
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(9)} + 1px)`,
+  },
+  overflowX: 'hidden',
+});
+
+export const navigationOpenedMixinSmooth = (theme: Theme): CSSObject => ({
+  width: navigationWidth,
+  overflowX: 'hidden',
+  transition: theme.transitions.create('width', {
+    duration: theme.transitions.duration.enteringScreen,
+    easing: theme.transitions.easing.sharp,
+  }),
+});
+
+export const navigationClosedMixinSmooth = (theme: Theme): CSSObject => ({
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(9)} + 1px)`,
+  },
+  overflowX: 'hidden',
+  transition: theme.transitions.create('width', {
+    duration: theme.transitions.duration.leavingScreen,
+    easing: theme.transitions.easing.sharp,
+  }),
+});
