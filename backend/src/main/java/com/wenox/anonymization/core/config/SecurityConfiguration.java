65a043c964d1870aa024e719a1fc2438a986c933
Wenox <wenoxar@gmail.com>
2021-12-27 07:32:52 +0100

    chore: externalize configuration for unprotected resources

--- backend/src/main/java/com/wenox/anonymization/core/config/SecurityConfiguration.java
+++ backend/src/main/java/com/wenox/anonymization/core/config/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import com.wenox.anonymization.core.security.filter.JwtAuthorizationFilter;
 import com.wenox.anonymization.core.security.service.JwtService;
 import com.wenox.anonymization.core.security.service.JwtUserDetailsService;
 import com.wenox.anonymization.core.security.filter.RequestLoggingFilter;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -18,11 +20,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@PropertySource(value = "secrets.properties", ignoreResourceNotFound = true)
+@PropertySource(value = "secrets.properties")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(jsr250Enabled = true, securedEnabled = true, prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Value("${core.api.unprotectedResources}")
+  private String[] unprotectedResources;
 
   private final JwtUserDetailsService userDetailsService;
   private final JwtService jwtService;
@@ -62,13 +67,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
 
     http.authorizeRequests()
-        .antMatchers(
-            "/api/v1/auth/**",
-            "/api/v1/users/register/**",
-            "/api/v1/users/verify-mail/**",
-            "/api/v1/reset-password/**",
-            "/api/v1/me/restore-account/**"
-        )
+        .antMatchers(unprotectedResources)
         .permitAll()
         .anyRequest()
         .authenticated()
