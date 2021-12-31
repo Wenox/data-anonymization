be106d33711afe276ae30698ea60d45c902ca745
Wenox <wenoxar@gmail.com>
2021-12-31 10:58:11 +0100

    feat: psql client can now communicate with remote psql server

--- backend/src/main/java/com/wenox/anonymization/uploader/restorer/DatabaseRestorer.java
+++ backend/src/main/java/com/wenox/anonymization/uploader/restorer/DatabaseRestorer.java
@@ -3,17 +3,21 @@ package com.wenox.anonymization.uploader.restorer;
 import com.wenox.anonymization.core.service.ProcessExecutorFactory;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DatabaseRestorer {
 
+  @Value("${uploader.templates.path}")
+  private String templatesPath;
+
   public void restorePostgresDatabase(final String templateName, final String dbName)
       throws IOException, InterruptedException, TimeoutException {
 
-    final String dbPath = "E:/anon/data-anonymization/stored_files/" + templateName;
+    final String dbPath = templatesPath + templateName;
 
-    ProcessExecutorFactory.newProcess("createdb", "-U", "postgres", "-T", "template0", dbName).execute();
-    ProcessExecutorFactory.newProcess("pg_restore", "-U", "postgres", "-d", dbName, "-v", dbPath).execute();
+    ProcessExecutorFactory.newProcess("createdb", "-h", "anonymisation_postgres_db", "-U", "postgres", "--no-password", "-T", "template0", dbName).execute();
+    ProcessExecutorFactory.newProcess("pg_restore",  "-h", "anonymisation_postgres_db", "-U", "postgres", "--no-password", "-d", dbName, "-v", dbPath).execute();
   }
 }
