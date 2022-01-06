6644b15690444075be9fe156ed16545df94eacb7
Wenox <wenoxar@gmail.com>
2022-01-06 22:06:42 +0100

    refactor: restorer improved logging

--- backend/src/main/java/com/wenox/uploading/restorer/DatabaseRestorer.java
+++ backend/src/main/java/com/wenox/uploading/restorer/DatabaseRestorer.java
@@ -3,6 +3,8 @@ package com.wenox.uploading.restorer;
 import com.wenox.infrastructure.service.ProcessExecutorFactory;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -21,21 +23,51 @@ public class DatabaseRestorer {
   @Value("${POSTGRES_HOST_PORT:5007}")
   private String postgresHostPort;
 
+  private static final Logger log = LoggerFactory.getLogger(DatabaseRestorer.class);
+
   public void restorePostgresDatabase(final String templateName, final String dbName)
       throws IOException, InterruptedException, TimeoutException {
 
     final String dbPath = templatesPath + "/" + templateName;
 
+    log.info("Restoring {} from {} for template {}.", dbName, dbPath, templateName);
+
     if (isRunningOnCloud) {
-      ProcessExecutorFactory.newProcess("createdb", "-h", postgresIpAddress, "-U", "postgres", "--no-password", "-T",
-          "template0", dbName).execute();
-      ProcessExecutorFactory.newProcess("pg_restore", "-h", postgresIpAddress, "-U", "postgres", "--no-password", "-d",
-          dbName, "-v", dbPath).execute();
+      ProcessExecutorFactory.newProcess(
+          "createdb",
+          "-h", postgresIpAddress,
+          "-U", "postgres", "--no-password",
+          "-T", "template0",
+          dbName
+      ).execute();
+      ProcessExecutorFactory.newProcess(
+          "pg_restore",
+          "-h", postgresIpAddress,
+          "-U", "postgres", "--no-password",
+          "-d", dbName,
+          "-v",
+          dbPath
+      ).execute();
     } else {
-      ProcessExecutorFactory.newProcess("createdb", "-h", postgresIpAddress, "-p", postgresHostPort, "-U", "postgres",
-          "--no-password", "-T", "template0", dbName).execute();
-      ProcessExecutorFactory.newProcess("pg_restore", "-h", postgresIpAddress, "-p", postgresHostPort, "-U", "postgres",
-          "--no-password", "-d", dbName, "-v", dbPath).execute();
+      ProcessExecutorFactory.newProcess(
+          "createdb",
+          "-h", postgresIpAddress,
+          "-p", postgresHostPort,
+          "-U", "postgres", "--no-password",
+          "-T", "template0",
+          dbName
+      ).execute();
+      ProcessExecutorFactory.newProcess(
+          "pg_restore",
+          "-h", postgresIpAddress,
+          "-p", postgresHostPort,
+          "-U", "postgres", "--no-password",
+          "-d", dbName,
+          "-v",
+          dbPath
+      ).execute();
     }
+
+    log.info("Restored {} successfully.", dbName);
   }
 }
