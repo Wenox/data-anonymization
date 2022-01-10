8448bc811d325b90b57e335def21179fd8c32f9f
Wenox <wenoxar@gmail.com>
2022-01-10 13:43:30 +0100

    feat: tokenization service

--- backend/src/main/java/com/wenox/processing/service/operations/TokenizationService.java
+++ backend/src/main/java/com/wenox/processing/service/operations/TokenizationService.java
@@ -1,5 +1,8 @@
 package com.wenox.processing.service.operations;
 
+import static java.util.stream.Collectors.toList;
+
+
 import com.wenox.anonymisation.domain.Tokenization;
 import com.wenox.processing.domain.Pair;
 import java.util.List;
@@ -14,13 +17,21 @@ public class TokenizationService {
     int count = 0;
     Map<String, Integer> valuesMap = new TreeMap<>();
     for (var value : rawValues) {
+      if (value == null) {
+        continue;
+      }
       var token = valuesMap.get(value);
       if (token == null) {
         valuesMap.put(value, ++count);
       }
     }
 
-    return rows.stream().map(row -> Pair.of(row.getFirst(), String.valueOf(valuesMap.get(row.getSecond())))).toList();
+    return rows.stream().map(row -> {
+      if (row.getSecond() == null) {
+        return Pair.of(row.getFirst(), String.valueOf(0));
+      }
+      var token = valuesMap.get(row.getSecond());
+      return Pair.of(row.getFirst(), String.valueOf(token));
+    }).toList();
   }
-
 }
